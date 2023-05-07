37740bf130d0c5a15c6d141ec16c5b6f07879791
Joven Tan <joven@old.st>
2023-05-08 01:14:14 +0800

    ui adjustments

--- components/Todo.vue
+++ components/Todo.vue
@@ -6,18 +6,22 @@
       <div class="divide-y divide-gray-200">
         <div v-for="todo in todos" :key="todo.id" class="relative flex items-start pb-4 pt-3.5">
           <div class="min-w-0 flex-1 text-sm leading-6">
-            <label :for="'todo-' + todo.id" class="font-medium text-gray-900">{{ todo.title }}</label>
+            <label :for="'todo-' + todo.id" :class="getTitleClass(todo.priority)" class="font-medium">{{ getEmoji(todo.status) }} {{ todo.title }}</label>
             <p :id="'todo-' + todo.id + '-description'" class="text-gray-500">{{ todo.notes }}</p>
-            <p class="text-gray-400 italic">Status: {{ todo.status }}</p>
+            <div class="w-full h-2 bg-gray-300 rounded">
+              <div :style="{ width: getBarWidth(todo) }" class="mt-4 h-2 bg-green-400   rounded"></div>
+            </div>
+            <p class="text-gray-700 mt-2">Duration: {{ formatTime(todo.duration) }} | Time Spent: {{ formatTime(todo.time_spent) }}</p>
           </div>
-          <div class="ml-3 flex h-6 items-center">
+          <div class="ml-3 flex h-6 items-center">  
             <input :id="'todo-' + todo.id" :aria-describedby="'todo-' + todo.id + '-description'" name="todos" type="checkbox" class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600" :checked="isDone(todo)" @change="toggleStatus(todo)" />
-          </div>  
+          </div>
         </div>
       </div>
     </fieldset>
   </div>
 </template>
+
 
 <script setup>
 import { inject } from 'vue';
@@ -30,4 +34,42 @@ const toggleStatus = (todo) => {
   todo.status = isDone(todo) ? 'todo' : 'done';
 };
 
+const getTitleClass = (priority) => {
+  switch (priority) {
+    case 'low':
+      return 'text-gray-400';
+    case 'medium':
+      return 'text-black-500';
+    case 'high':
+      return 'text-red-500';
+    default:
+      return 'text-black-900';
+  }
+};
+
+const getEmoji = (status) => {
+  switch (status) {
+    case 'todo':
+      return '📝';
+    case 'in_progress':
+      return '🚧';
+    case 'done':
+      return '✅';
+    default:
+      return '';
+  }
+};
+
+const getBarWidth = (todo) => {
+  const percentage = Math.min((todo.time_spent / todo.duration) * 100, 100);
+  return `${percentage}%`;
+};
+
+const formatTime = (minutes) => {
+  const hours = Math.floor(minutes / 60);
+  const remainingMinutes = minutes % 60;
+  return hours > 0
+    ? `${hours}h ${remainingMinutes}m`
+    : `${remainingMinutes}m`;
+};
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -29,7 +29,7 @@
           <div class="absolute left-0 flex-shrink-0 lg:static">
             <a href="#">
               <span class="sr-only">Your Company</span>
-              <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=300" alt="Your Company" />
+              <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=green&shade=300" alt="TodoBud" />
             </a>
           </div>
 
